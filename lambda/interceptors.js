447320d64b9800f394c4bfb327de63763327eb0c
kamesh rathore <kamesh.kr258@gmail.com>
2020-08-11 15:43:57 +0530

    request response interceptor definition added for alxa skill

---
+++ lambda/interceptors.js
@@ -0,0 +1,74 @@
+const Alexa = require('ask-sdk-core');
+// i18n dependency
+const i18n = require('i18next');
+const languageStrings = require('./localisation');
+
+// This request interceptor will log all incoming requests to this lambda
+const LoggingRequestInterceptor = {
+    process(handlerInput) {
+        console.log(`Incoming request: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+    }
+};
+
+// This response interceptor will log all outgoing responses of this lambda
+const LoggingResponseInterceptor = {
+    process(handlerInput, response) {
+        console.log(`Outgoing response: ${JSON.stringify(response)}`);
+    }
+};
+
+// This request interceptor will bind a translation function 't' to the handlerInput
+// Additionally it will handle picking a random value if instead of a string it receives an array
+const LocalisationRequestInterceptor = {
+    process(handlerInput) {
+        const localisationClient = i18n.init({
+            lng: Alexa.getLocale(handlerInput.requestEnvelope),
+            resources: languageStrings,
+            returnObjects: true
+        });
+        localisationClient.localise = function localise() {
+            const args = arguments;
+            const value = i18n.t(...args);
+            if (Array.isArray(value)) {
+                return value[Math.floor(Math.random() * value.length)];
+            }
+            return value;
+        };
+        handlerInput.t = function translate(...args) {
+            return localisationClient.localise(...args);
+        }
+    }
+};
+
+
+const LoadTimezoneRequestInterceptor = {
+    async process(handlerInput) {
+        const {attributesManager, serviceClientFactory, requestEnvelope} = handlerInput;
+        const sessionAttributes = attributesManager.getSessionAttributes();
+        const deviceId = Alexa.getDeviceId(requestEnvelope);
+
+        if (!sessionAttributes['timezone']){
+            // let's try to get the timezone via the UPS API
+            // (no permissions required but it might not be set up)
+            try {
+                const upsServiceClient = serviceClientFactory.getUpsServiceClient();
+                const timezone = await upsServiceClient.getSystemTimeZone(deviceId);
+                if (timezone) { // the user might not have set the timezone yet
+                    console.log('Got timezone from device: ' + timezone);
+                    //save to session and persisten attributes
+                    sessionAttributes['timezone'] = timezone;
+                }
+            } catch (error) {
+                console.log(JSON.stringify(error));
+            }
+        }
+    }
+};
+
+module.exports = {
+    LoggingRequestInterceptor,
+    LoggingResponseInterceptor,
+    LocalisationRequestInterceptor,
+    LoadTimezoneRequestInterceptor
+}
+
